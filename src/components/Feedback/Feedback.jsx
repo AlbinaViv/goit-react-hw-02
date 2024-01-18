7a98506f5925d8652ed20fffc8621870990b1890
Albina Vivdenko <albinka3322@gmail.com>
2024-01-18 16:43:51 +0100

    button + value

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,34 @@
+// import { useState } from "react";
+
+// export const Feedback = ({ value, onUpdate }) => {
+//   const [good, setGood] = useState(0);
+
+//   const [neutral, setNeutral] = useState(0);
+
+//   const [bad, setBad] = useState(0);
+
+//   const handleClickGood = () => {
+//     setGood(good + 1);
+//   };
+
+//   const handleClickNeutral = () => {
+//     setNeutral(neutral + 1);
+//   };
+
+//   const handleClickBad = () => {
+//     setBad(bad + 1);
+//   };
+
+// return (
+//   <div>
+//     <ul>
+//       <li>{/* <span onClick={handleClickGood}>Good: {good} </span> */}</li>
+//       <li>
+//         {/* <span onClick={handleClickNeutral}>Neutral: {neutral}</span> */}
+//       </li>
+//       <li>{/* <span onClick={handleClickBad}>Bad: {bad}</span> */}</li>
+//       <li>{/* <span onClick={onUpdate}>Total: {value} </span> */}</li>
+//     </ul>
+//   </div>
+// );
+// };
