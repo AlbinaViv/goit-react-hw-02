4c4977f19b8ff20f4558f0507581657cee13f5f3
Albina Vivdenko <albinka3322@gmail.com>
2024-01-22 15:33:42 +0100

    all

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,34 +1,21 @@
-// import { useState } from "react";
-
-// export const Feedback = ({ value, onUpdate }) => {
-//   const [good, setGood] = useState(0);
-
-//   const [neutral, setNeutral] = useState(0);
-
-//   const [bad, setBad] = useState(0);
-
-//   const handleClickGood = () => {
-//     setGood(good + 1);
-//   };
-
-//   const handleClickNeutral = () => {
-//     setNeutral(neutral + 1);
-//   };
-
-//   const handleClickBad = () => {
-//     setBad(bad + 1);
-//   };
-
-// return (
-//   <div>
-//     <ul>
-//       <li>{/* <span onClick={handleClickGood}>Good: {good} </span> */}</li>
-//       <li>
-//         {/* <span onClick={handleClickNeutral}>Neutral: {neutral}</span> */}
-//       </li>
-//       <li>{/* <span onClick={handleClickBad}>Bad: {bad}</span> */}</li>
-//       <li>{/* <span onClick={onUpdate}>Total: {value} </span> */}</li>
-//     </ul>
-//   </div>
-// );
-// };
+export const Feedback = ({ good, neutral, bad, total, positive }) => {
+  return (
+    <ul>
+      <li>
+        <span>Good: {good} </span>
+      </li>
+      <li>
+        <span>Neutral: {neutral}</span>
+      </li>
+      <li>
+        <span>Bad: {bad}</span>
+      </li>
+      <li>
+        <span>Total: {total}</span>
+      </li>
+      <li>
+        <span>Positive: {positive}%</span>
+      </li>
+    </ul>
+  );
+};
